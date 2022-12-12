3413fa70ab58454050be3775e27b8e0ed867cb4c
OlenaBern <112780284+OlenaBern@users.noreply.github.com>
2022-12-12 08:49:45 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict"
+
+// Крапка з комою
+// Все, що відкрито - має буди закрито
+
+// console.log('Я вчу JS');
+// 1
+// console.log('Вчимо JS')
+// 2
+// console.log('JS')
+// console.log('Вчимо')
+
+// 3 
+// console.log('JS') console.log('Вчимо')
+
+// 4 
+// console.log('JS);
+// console.log('Вчимо');
+
+
+//Змінні та константи (ключові слова let, const, var)
+// let userColorEyes;
+// let user;
+// let userName = 'Вася';
+// user = userName;
+// console.log(user);
+
+// let myage = 36;
+// console.log(myage)
+
+// const BLOCK_SIZE = 14 + 50;
+// console.log(BLOCK_SIZE)
+// const BLOCK_HEIGHT = 100;
+// console.log(BLOCK_HEIGHT);
+// if (true) {
+// 	var size = 15;
+// }
+// console.log(size);
+
+// let userAge = 36;
+// let userInfo = `Фрілансер ${userAge}`;
+// console.log(userInfo);
+
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+
+// let userName;
+// console.log(typeof userName);
+
+// let userSize = "15" / "8";
+// console.log(typeof userSize)
